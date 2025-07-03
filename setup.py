d73799f3f1b196ff224def55488a5d3ef530c199
Uchida16104 <Uchida16104@users.noreply.github.com>
2025-07-03 06:23:11 +0000

    deploy: 3d995ffbba35826c26d4d852ee82620d47e46f6f

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crearte",
-    version="3.3.0",
+    version="3.4.0",
     description="Visual Music Score Generator - Python CLI",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
