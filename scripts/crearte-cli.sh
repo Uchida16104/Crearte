ae073be40f32881c55b089b080a9a44cc2de5e70
Uchida16104 <Uchida16104@users.noreply.github.com>
2025-07-03 00:27:02 +0000

    deploy: bfae5766891fd17142ba52cd04feff3b2fc56f13

---
+++ scripts/crearte-cli.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+echo "→ Installing dependencies..."
+python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt
+echo "Please Install SuperCollider / Processing manually."
+echo "→ Done!" 
